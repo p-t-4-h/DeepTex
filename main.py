fd5b90968702c1705d4ca655922fc55ccf6af88c
ρƚ4ԋ <61159937+p-t-4-h@users.noreply.github.com>
2024-01-28 18:53:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,11 @@
 from PIL import Image, ImageOps
-import sys
 import numpy as np
 from tqdm import tqdm
-import threading
+from multiprocessing import Pool
 
 #numpy.set_printoptions(threshold=sys.maxsize)
 horizontal = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
 vertical = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
-
-class Image_Processing(threading.Thread):
-
-  def __init__(self, file: str, output=False):
-    threading.Thread.__init__(self)
-    self.file = file
-    self.output = output
-    # set a default value
-    self.ret = None
-
-  def run(self):
-    np_d = prepare(self.file, self.output)
-
-    #Convoluting arrays
-    h = conv(np_d, horizontal)
-    v = conv(np_d, vertical)
-
-    #Pooling arrays
-    k_size = (2, 2)
-    h_p = max_pool(h, k_size)
-    v_p = max_pool(v, k_size)
-
-    if self.output:
-      data = Image.fromarray(h).convert("L")
-      data.save(f"./Outputs/h_{self.file}")
-      data = Image.fromarray(v).convert("L")
-      data.save(f"./Outputs/v_{self.file}")
-      data = Image.fromarray(h_p).convert("L")
-      data.save(f"./Outputs/h_p_{self.file}")
-      data = Image.fromarray(v_p).convert("L")
-      data.save(f"./Outputs/v_p_{self.file}")
-
-    self.ret = (h_p, v_p)
-
 
 def conv(Input, Filter):
   filter_size = Filter.shape[0]
@@ -100,13 +65,37 @@ def max_pool(matrice, k_size):
   return matrice[:mk * k_x, :my * k_y].reshape(mk, k_x, my,
                                                k_y).max(axis=(1, 3))
 
+def Process(file: str, output=True):
+  np_d = prepare(file, output)
+
+  #Convoluting arrays
+  h = conv(np_d, horizontal)
+  v = conv(np_d, vertical)
+
+  #Pooling arrays
+  k_size = (2, 2)
+  h_p = max_pool(h, k_size)
+  v_p = max_pool(v, k_size)
+
+  if output:
+    data = Image.fromarray(h).convert("L")
+    data.save(f"./Outputs/h_{file}")
+    data = Image.fromarray(v).convert("L")
+    data.save(f"./Outputs/v_{file}")
+    data = Image.fromarray(h_p).convert("L")
+    data.save(f"./Outputs/h_p_{file}")
+    data = Image.fromarray(v_p).convert("L")
+    data.save(f"./Outputs/v_p_{file}")
+
+  return (h_p, v_p)
 
 def start_processing(img_l, output=True):
-  threads = [Image_Processing(img, output) for img in img_l]
-  for thread in threads:
-    thread.start()
-    
-  for thread in threads:
-    thread.join()    
+  pool = Pool(10)
+  results = [pool.apply_async(Process, (img, output)) for img in img_l]
+  pool.close()
+  pool.join()
 
-start_processing(["Numbers.png", "Maths.jpg"], output=True)
+  print([result.get() for result in results])
+
+if __name__ == "__main__":
+  start_processing(["Numbers.png", "Maths.jpg"], output=True)
