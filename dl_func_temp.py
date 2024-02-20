7b05c48505dd2a73b153ce9283bdcc3444f5604a
Achilou93 <156782286+Achilou93@users.noreply.github.com>
2024-02-20 09:34:33 +0100

    Update dl_func_temp.py

--- dl_func_temp.py
+++ dl_func_temp.py
@@ -1,7 +1,6 @@
 import numpy
 
 eps=1/100
-
 def lrelu(x):
     if x<0:
         return x*eps
@@ -14,7 +13,6 @@ def initialisation(dimensions):
     for c in range(1,c):
         parametres["W"+str(c)]=np.random.randn(dimensions[c],dimensions[c-1])
         parametres["b"+str(c)]=np.random.randn(dimensions[c],1)
-
     return parametres
 
 def forward_propagation(x, parametres):
@@ -30,13 +28,13 @@ def back_propagation(y,activations,parametres):
     C=len(parametres)//2
     dZ=activations["A"+str(C)]-y
     gradients={}
-
     for c in reversed(range(1,C+1)):
         gradients["dW"+str(c)]=1/m*np.dot(dZ, activations["A"+str(c-1)].T)
         gradients["dW"+str(c)]=1/m*np.sum(dZ,axis=1,keepdims=True)
         if c>1:
             dZ = np.dot(parametres['W'+str(c)].T,dZ)*activations['A'+str(c-1)]*(1-activations['A'+str(c-1)])
     return gradients
+    
 def update(gradients, parametres, learning_rate):
     C = len(parametres) // 2
     for c in range(1, C + 1):
@@ -49,3 +47,5 @@ def predict(X, parametres):
     C = len(parametres) // 2
     Af = activations['A' + str(C)]
     return Af >= 0.5
+
+print(parametres)
