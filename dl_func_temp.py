7aa4a10515049df0419675bbf80875abc2e7e88c
Achilou93 <156782286+Achilou93@users.noreply.github.com>
2024-02-20 09:28:16 +0100

    Update dl_func_temp.py
    fonctions dl
    bp+acc

--- dl_func_temp.py
+++ dl_func_temp.py
@@ -1,8 +1,10 @@
 import numpy
 
+eps=1/100
+
 def lrelu(x):
     if x<0:
-        return x*1/100
+        return x*eps
     else:
         return x
 
@@ -16,7 +18,7 @@ def initialisation(dimensions):
     return parametres
 
 def forward_propagation(x, parametres):
-    activations={"A0"=X}
+    activations={"A0":X}
     C=len(parametres) // 2
     for c in range(1,C+1):
         Z=parametres["W"+str(c)].dot(activations["A"+str(c-1)])+parametres["b"+str(c)]
@@ -31,3 +33,19 @@ def back_propagation(y,activations,parametres):
 
     for c in reversed(range(1,C+1)):
         gradients["dW"+str(c)]=1/m*np.dot(dZ, activations["A"+str(c-1)].T)
+        gradients["dW"+str(c)]=1/m*np.sum(dZ,axis=1,keepdims=True)
+        if c>1:
+            dZ = np.dot(parametres['W'+str(c)].T,dZ)*activations['A'+str(c-1)]*(1-activations['A'+str(c-1)])
+    return gradients
+def update(gradients, parametres, learning_rate):
+    C = len(parametres) // 2
+    for c in range(1, C + 1):
+        parametres['W'+str(c)]=parametres['W'+str(c)]-learning_rate*gradients['dW'+str(c)]
+        parametres['b'+str(c)]=parametres['b'+str(c)]-learning_rate*gradients['db'+str(c)]
+    return parametres
+
+def predict(X, parametres):
+    activations = forward_propagation(X, parametres)
+    C = len(parametres) // 2
+    Af = activations['A' + str(C)]
+    return Af >= 0.5
